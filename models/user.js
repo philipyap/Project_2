2c2bdaccdfee26c0ae5d132636f3350d595603eb
Philip Yap <philipyap@Philips-MacBook.local>
2020-08-13 12:29:24 -0700

    [authapp]added bcrypt

---
+++ models/user.js
@@ -0,0 +1,67 @@
+'use strict';
+const bcrypt = require('bcrypt')
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  user.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1, 99],
+          msg: 'Name must be 1 to 99 characters'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg:'Invalid email'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 99],
+          msg: 'password must be between 8 and 99 characters'
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'user',
+  });
+  user.addHook('beforeCreate', function(pendingUser) {
+    // hash the password for us
+    let hash = bcrypt.hashSync(pendingUser.password, 12)
+
+    pendingUser.password = hash
+
+  })
+  user.prototype.validPassword = function(passwordTyped){
+    let correctPassword = bcrypt.compareSync(passwordTyped, this.password) 
+    return correctPassword;
+  }
+
+  user.prototype.toJSON = function(){
+    let userData = this.get()
+    delete userData.password
+    return userData
+  }
+
+  return user;
+};
